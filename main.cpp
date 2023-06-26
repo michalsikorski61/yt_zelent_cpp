387e996f59c08508951b9903c42cad6556bfd02b
Michał Sikorski <michalsikorski@gmail.com>
2023-06-26 20:29:46 +0200

    Hello world

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <iostream> // biblioteka - input output stream
 
-using namespace std;
+using namespace std; // przestrzeń nazw, by nie trzeba było pisać np. std::cout czy std::endl
 
-int main()
-{
+int main(){//główna fn zawsze obecna, od niej się zaczyna program
+    cout << "Hello world!"<<endl;//<< operatory wysyłania do strumienia
+    //endl - przejście do nowej linii
+    //cout - wypisz coś do konsoli console output
+    return 0; // koniec zrobiłam wszystko i oddaje kontrolę do nadrzędnego procesu
     
-    cout<<"Siema";
 }
