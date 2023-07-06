2cb00c77e8e488146c955b335bf5dee334596c89
Michał Sikorski <michalsikorski@gmail.com>
2023-07-06 08:02:53 +0200

    Program JEBUT

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
-#include <iostream> 
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
 
-using namespace std; 
+using namespace std;
 
-int wiek;
+int main()
+{
 
-int main(){
-
-    cout<<"Ile masz lat: ";
-    cin>>wiek;
-
-    if(wiek< 18){
-        cout<<"Jestes niepelnoletni i nie mozesz zostac prezydentem ";
-    }else if(wiek <35){
-        cout<<"Jestes pelnoletni, ale nie mozesz zostac prezydentem";
-    }else{
-        cout<<"Jestes pelnoletni i mozesz zostac prezydentem";
+    for (int i = 15; i >= 0; i--)
+    {
+        Sleep(1000);
+        system("cls");
+        cout << i << endl;
     }
-    return 0; 
+    cout << "JEBUT";
 
+    return 0;
 }
